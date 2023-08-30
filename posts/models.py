d2870dd37d4176b36b423f67a68e74e921151b6e
KimHyunJung <hday10166@gamil.com>
2023-08-30 09:32:49 +0900

    모델링과 마이그레이션

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True) #생성시간
+    updated_at = models.DateTimeField(auto_now=True) #수정시간
+    image = models.ImageField(upload_to='image/')
