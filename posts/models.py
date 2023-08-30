8c2ef60d25cbbc1a243da11c928df16fec5f2e02
KimHyunJung <hday10166@gamil.com>
2023-08-30 09:51:31 +0900

    Post 모델링

--- posts/models.py
+++ posts/models.py
@@ -6,4 +6,4 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True) #생성시간
     updated_at = models.DateTimeField(auto_now=True) #수정시간
-    image = models.ImageField(upload_to='image/')
+    image = models.ImageField(upload_to='image/%Y/%m')
