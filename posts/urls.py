a512da8476dbcefa8470cd8ed5bdbfe4aa26115f
KimHyunJung <hday10166@gamil.com>
2023-08-30 10:55:39 +0900

    Post Create GET 요청 기능 구현

--- posts/urls.py
+++ posts/urls.py
@@ -6,5 +6,7 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    
     
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 
 
@@ -10,3 +11,18 @@ def index(request):
         'posts': posts,
     }
     return render(request, 'index.html', context)
+
+
+
+def create(request):
+    if  request.method == 'POST':
+        pass 
+    else:
+        form = PostForm()
+        
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'form.html', context)
