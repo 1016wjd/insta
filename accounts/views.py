330785dab450810bdeb76d8de3a7f969b5b5b2de
KimHyunJung <hday10166@gamil.com>
2023-08-30 13:13:55 +0900

    유저모델링

---
+++ accounts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import User
+# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        pass
+    else:
+        pass
