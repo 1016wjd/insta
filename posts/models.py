6c314211f1dcad48dc6e7af2e35608c78c1fbf1a
KimHyunJung <hday10166@gamil.com>
2023-08-30 11:41:44 +0900

    resized

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,14 @@
 from django.db import models
-
+from django_resized import ResizedImageField
 # Create your models here.
 
 class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True) #생성시간
     updated_at = models.DateTimeField(auto_now=True) #수정시간
-    image = models.ImageField(upload_to='image/%Y/%m')
+    # image = models.ImageField(upload_to='image/%Y/%m')
+    image = ResizedImageField(
+        size=[500,500],
+        crop=['middle','center'],
+        upload_to='image/%Y/%m'
+    )
