a9f84b72826dbac3a37549ba33ed42f4ec1ea6cd
KimHyunJung <hday10166@gamil.com>
2023-08-30 14:09:45 +0900

    login

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import User
 from django.contrib.auth import get_user_model
 
@@ -8,3 +8,7 @@ class CustomUserCreationForm(UserCreationForm):
         model = get_user_model() # 유지보수가 더 편한 방법 
         # model = User
         fields = ('username', 'profile_image', )
+
+    
+class CustomAuthenticationForm(AuthenticationForm):
+    pass
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import User
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.contrib.auth import login as auth_login
 # Create your views here.
 
 def signup(request):
@@ -16,3 +17,19 @@ def signup(request):
         'form': form,
     }
     return render(request, 'accounts/form.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('posts:index')
+
+    else:
+         form = CustomAuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/form.html', context) 
